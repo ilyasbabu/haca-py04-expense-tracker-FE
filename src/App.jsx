c925a0048180c2e80fe13c5d0678039acf0d0f29
ilyasbabu <ilyasbabuvp@gmail.com>
2026-04-08 19:24:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import ExpenseTracker from "./components/HomeUi"
+import Login from "./components/LoginUi"
+
+function App() {
+  const [user, setUser] = useState(() => !!localStorage.getItem("access"))
+  return user ? <ExpenseTracker/> : <Login onLogin={() => setUser(true)} />
+}
+
+export default App
